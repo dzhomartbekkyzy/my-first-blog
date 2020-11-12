adbac113db38f21dad856149d18e4b2b9f3be746
dzhomartbekkyzy <dilia.zhomartbekkyzy@mail.ru>
2020-11-12 23:23:41 +0600

    deleteadded

--- blog/views.py
+++ blog/views.py
@@ -38,3 +38,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
